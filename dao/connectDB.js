d2db964e419bd7d568457a845c4ecce77fe7bf71
Larry_A <larryagbana@gmail.com>
2020-05-04 16:39:17 -0400

    added database multipleStatements property

--- dao/connectDB.js
+++ dao/connectDB.js
@@ -1,25 +1,20 @@
+// Import database connection and CLI table dependencies
 const mysql = require("mysql2/promise")
 
+/*
+Function: sets database connection parameters and establishes connection to requested database
 
+*@param database {String} name of MySQL database the user connects to
+*/
 async function connect(database) {
     const connectionOptions = {
         host: 'localhost',
         port: 3306,
         user: "root",
         database,
-        password: "Jameslives@2091"
+        password: "Jameslives@2091",
+        multipleStatements: true
     }
-
     return await mysql.createConnection(connectionOptions)
-    // .then(connection => {
-    //     console.log(`connected to DB as id: ${connection.threadId}`)
-    // })
-    // .catch(err => {
-    //     console.log('error connecting to DB: ' + err.stack)
-    //     process.exit(1) // Quit node if there is no DB connection
-    // })
-
 }
-
-
 module.exports = connect
