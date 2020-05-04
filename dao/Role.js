6ba68879cecb82faeb8fc0d41322210df876ca3b
Larry_A <larryagbana@gmail.com>
2020-05-04 09:43:06 -0400

    class method: addRole, adds a new role to roles table

--- dao/Role.js
+++ dao/Role.js
@@ -15,7 +15,26 @@ class Role {
         this.salary = options.salary
         this.departmentID = options.departmentID
     }
-
+        /*
+     addDept method: 
+     - creates connection to database: "human_Resources_DB"
+     - executes create department query, with class @param: this.departmentName
+     - catches any errors, and ends the connection.
+    */
+   async addRole() {
+    try {
+        const db = await this.connection("human_Resources_DB")
+        const action = await db.query(`INSERT INTO roles (title, salary, department_ID) VALUES ("${this.title}", "${this.salary}", "${this.departmentID}");`)
+        console.log(`
+            New role added to the database: ${this.title}
+        `)
+    } catch (err) {
+        console.log(err)
+    } finally {
+        const db = await this.connection("human_Resources_DB")
+        db.end()
+    }
+}
     /*
      getRolesIds method: 
     - creates connection to database: "human_Resources_DB"
@@ -40,9 +59,34 @@ class Role {
         db.end()
     }
 }
+/*
+    getRoleNames method: 
+    - creates connection to database: "human_Resources_DB"
+    - executes SELECT query on the roles table
+     *return = []
+        # array of all role Names, to be used for user selection of roles to delete
+    - catches any errors, and ends the connection.
+    */
+//    async getRoleNames() {
+//     try {
+//         let result = []
+//         let db = await this.connection("human_Resources_DB")
+//         let [response] = await db.query(`SELECT department_name FROM role`)
+//         for (let row of response) {
+//             result.push(row.department_name)
+//         }
+//         return result
+
+//     } catch (err) {
+//         console.log(err)
+//     } finally {
+//         const db = await this.connection("human_Resources_DB")
+//         db.end()
+//     }
+// }
 }
 
-const test = new Role()
-test.getRolesIds()
-// console.log(arr)
+const test = new Role({title: "Test Engineer", salary: 100000, departmentID: 2})
+test.addRole()
+
 
