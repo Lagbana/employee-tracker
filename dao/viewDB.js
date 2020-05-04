500bf1a27665924946c6055ef986f42516a99211
Larry_A <larryagbana@gmail.com>
2020-05-04 16:35:06 -0400

    refactored code and updated comments

--- dao/viewDB.js
+++ dao/viewDB.js
@@ -1,43 +1,30 @@
-const cTable = require('console.table')
-// const mysql = require('mysql2/promise')
+// Import database connection and CLI table dependencies
 const connection = require('./connectDB')
+const cTable = require('console.table')
 
+/*
+viewDB returns a CLI table from the human_Resources_DB database
 
-
-const viewDB = async (tableName, value) => {
-    
+*@param tableName {String} is the name of the table the user wants to view
+*@param property {String} is the column name the user wants to order the table by
+*/
+const viewDB = async (tableName, property) => {
     try {
-        const db = await connection("db_table")
-        const [tbRows] = await db.query(`SELECT * FROM ${tableName} ORDER BY ${value} ASC`)
-        const table = cTable.getTable(tbRows)
-        console.log(`
+        const db = await connection("human_Resources_DB")
+        const [tbRows] = await db.query(`SELECT * FROM ${tableName} ORDER BY ${property} ASC`)
+        const table = await cTable.getTable(tbRows)
+        return (`
+
 ${table}
+
             `)
 
     } catch (err) {
         console.log(err)
     } finally {
-        const db = await connection("db_table")
+        const db = await connection("human_Resources_DB")
         db.end()
     }
 }
 
-// ------------------------------------
-//  Working Commands for single tables
-// ------------------------------------
-
-// viewDB('db_table', 'ID')
-// viewDB('db_table', 'Title')
-// viewDB('db_table', 'Manager')
-// viewDB('db_table', 'Department')
-
-
-// ------------------------------------
-//  Working Commands for single tables
-// ------------------------------------
-
-// viewDB('employees', 'id')
-// viewDB('roles', 'id')
-// viewDB('departments', 'id')
-
-module.exports = viewDB()
+module.exports = viewDB
