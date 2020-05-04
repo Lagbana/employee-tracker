7b837e126556b6cd1d609379e00bf1d9c4a85d0f
Larry_A <larryagbana@gmail.com>
2020-05-03 20:05:08 -0400

    updated database queries

--- seeds.sql
+++ seeds.sql
@@ -1,7 +1,7 @@
 /* Seeds for SQL table. We haven't discussed this type of file yet */
-USE humanResourcesDB;
+USE human_resourcesDB;
 
--- Department Names --
+-- Create Departments --
 INSERT INTO departments (department_name)
 VALUES ("Human Resources");
 
@@ -14,66 +14,83 @@ VALUES ("Finance");
 INSERT INTO departments (department_name)
 VALUES ("Marketing");
 
--- All Roles --
+INSERT INTO departments (department_name)
+VALUES ("Sales");
+
+-- Create Roles --
 INSERT INTO roles (title, salary, department_ID)
-VALUES ("manager", 100000, 1);
+VALUES ("Manager", 100000, 1);
 
 INSERT INTO roles (title, salary, department_ID)
-VALUES ("manager", 150000, 2);
+VALUES ("Manager", 150000, 2);
 
 INSERT INTO roles (title, salary, department_ID)
-VALUES ("manager", 140000, 3);
+VALUES ("Manager", 140000, 3);
 
 INSERT INTO roles (title, salary, department_ID)
-VALUES ("manager", 150000, 4);
+VALUES ("Manager", 150000, 4);
 
 INSERT INTO roles (title, salary, department_ID)
-VALUES ("front end developer", 80000, 2);
+VALUES ("Front End Developer", 80000, 2);
 
 INSERT INTO roles (title, salary, department_ID)
-VALUES ("back end developer", 80000, 2);
+VALUES ("Back End Developer", 80000, 2);
 
 INSERT INTO roles (title, salary, department_ID)
-VALUES ("test engineer", 70000, 2);
+VALUES ("Test Engineer", 70000, 2);
 
 INSERT INTO roles (title, salary, department_ID)
-VALUES ("financial analyst", 70000, 3);
+VALUES ("Financial Analyst", 70000, 3);
 
 INSERT INTO roles (title, salary, department_ID)
-VALUES ("investment analyst", 80000, 3);
+VALUES ("Investment Analyst", 80000, 3);
 
 INSERT INTO roles (title, salary, department_ID)
-VALUES ("marketing analyst", 65000, 4);
+VALUES ("Marketing Analyst", 65000, 4);
 
 INSERT INTO roles (title, salary, department_ID)
-VALUES ("content writer", 60000, 4);
+VALUES ("Content Writer", 60000, 4);
 
 INSERT INTO roles (title, salary, department_ID)
-VALUES ("recruiter", 55000, 1);
+VALUES ("Recruiter", 55000, 1);
 
 INSERT INTO roles (title, salary, department_ID)
-VALUES ("human resources assistant", 45000, 1);
+VALUES ("Human Resources Assistant", 45000, 1);
 
 INSERT INTO roles (title, salary, department_ID)
-VALUES ("performance analyst", 60000, 1);
+VALUES ("Performance Analyst", 60000, 1);
+
+INSERT INTO roles (title, salary, department_ID)
+VALUES ("Manager", 150000, 5);
+
+INSERT INTO roles (title, salary, department_ID)
+VALUES ("Sales Representative", 80000, 5);
 
 /* EMPLOYEES */
--- Human Resources Department Employees --
+-- Managers --
 INSERT INTO employees (first_name, last_name, role_ID)
 VALUES ("Saul", "Berenson", 1 );
 
-INSERT INTO employees (first_name, last_name, role_ID, manager_ID)
-VALUES ("Ian", "Morgan", 10, 1);
+INSERT INTO employees (first_name, last_name, role_ID)
+VALUES ("Michael", "Scofield", 2 );
 
-INSERT INTO employees (first_name, last_name, role_ID, manager_ID)
-VALUES ("Tunde", "Ola", 11, 1);
+INSERT INTO employees (first_name, last_name, role_ID)
+VALUES ("Bobby", "Axelrod", 3 );
 
+INSERT INTO employees (first_name, last_name, role_ID)
+VALUES ("Don", "Draper", 4 );
+
+-- HR Team --
 INSERT INTO employees (first_name, last_name, role_ID, manager_ID)
 VALUES ("Ian", "Morgan", 12, 1);
 
--- Engineering Department Employees --
-INSERT INTO employees (first_name, last_name, role_ID)
-VALUES ("Michael", "Scofield", 2 );
+INSERT INTO employees (first_name, last_name, role_ID, manager_ID)
+VALUES ("Tunde", "Ola", 13, 1);
+
+INSERT INTO employees (first_name, last_name, role_ID, manager_ID)
+VALUES ("Ted", "Obama", 14, 1);
+
+-- Engineering --
 
 INSERT INTO employees (first_name, last_name, role_ID, manager_ID)
 VALUES ("Henry", "Cho", 5, 2);
@@ -84,50 +101,39 @@ VALUES ("Carlos", "Tevez", 6, 2);
 INSERT INTO employees (first_name, last_name, role_ID, manager_ID)
 VALUES ("Didier", "Toure", 7, 2);
 
--- Finance Department Employees --
-INSERT INTO employees (first_name, last_name, role_ID)
-VALUES ("Bobby", "Axelrod", 3 );
-
+-- Finance --
 INSERT INTO employees (first_name, last_name, role_ID, manager_ID)
 VALUES ("Amina", "Abdulrahman", 8, 3);
 
 INSERT INTO employees (first_name, last_name, role_ID, manager_ID)
-VALUES ("Cindy", "Ho", 13, 3);
+VALUES ("Cindy", "Ho", 9, 3);
+
+-- Marketing --
+INSERT INTO employees (first_name, last_name, role_ID, manager_ID)
+VALUES ("Amira", "Kaur", 10, 4);
 
 INSERT INTO employees (first_name, last_name, role_ID, manager_ID)
-VALUES ("Amira", "Kaur", 14, 3);
+VALUES ("Ben", "Taarabt", 11, 4);
 
--- Marketing Employees --
+-- Sales --
 INSERT INTO employees (first_name, last_name, role_ID)
-VALUES ("Don", "Draper", 4 );
+VALUES ("George", "Costanza", 15);
 
 INSERT INTO employees (first_name, last_name, role_ID, manager_ID)
-VALUES ("Ben", "Taarabt", 9, 4);
+VALUES ("Cosmo", "Kramer", 16, 15);
 
--- Get Engineering Manager name
-SELECT CONCAT(employees.first_name, " ", employees.last_name) AS "Manager"
-FROM ((departments
-LEFT JOIN roles ON departments.id = roles.department_ID)
-LEFT JOIN employees ON roles.id = employees.role_ID)
-WHERE roles.title = "Manager" AND departments.department_name = "Engineering";
 
--- Get Human Resources Manager name
-SELECT CONCAT(employees.first_name, " ", employees.last_name) AS "Manager"
-FROM ((departments
-LEFT JOIN roles ON departments.id = roles.department_ID)
-LEFT JOIN employees ON roles.id = employees.role_ID)
-WHERE roles.title = "Manager" AND departments.department_name = "Human Resources";
+-- View All --
+SELECT * FROM joined_table;
 
--- Get Finance Manager name
-SELECT CONCAT(employees.first_name, " ", employees.last_name) AS "Manager"
-FROM ((departments
-LEFT JOIN roles ON departments.id = roles.department_ID)
-LEFT JOIN employees ON roles.id = employees.role_ID)
-WHERE roles.title = "Manager" AND departments.department_name = "Finance";
+-- View All by Department --
+SELECT * FROM joined_table
+ORDER BY Department ASC;
 
--- Get Marketing Manager name
-SELECT CONCAT(employees.first_name, " ", employees.last_name) AS "Manager"
-FROM ((departments
-LEFT JOIN roles ON departments.id = roles.department_ID)
-LEFT JOIN employees ON roles.id = employees.role_ID)
-WHERE roles.title = "Manager" AND departments.department_name = "Marketing";
+-- View All by Roles --
+SELECT * FROM joined_table
+ORDER BY Title ASC;
+
+-- View All by Manager --
+SELECT * FROM joined_table
+ORDER BY Manager ASC;
