63e36ec8bad0015a247ebaf690b93b06e679f2fa
Larry_A <larryagbana@gmail.com>
2020-05-04 07:46:46 -0400

    class method: deleteDept,  deletes a department from database

--- dao/Department.js
+++ dao/Department.js
@@ -1,18 +1,28 @@
-
-// const cTable = require('console.table')
+// Import database connection dependency
 const connection = require('./connectDB')
 
+/*
+    Department Class allows the adding or deleting of a department
+
+    *@param constructor (departmentName), where departmentName = String 
+*/
 class Department {
+    // Connection module is inherited by default
     constructor(departmentName) {
         this.connection = connection
         this.departmentName = departmentName
     }
 
+    /*
+     addDept method: 
+     - creates connection to database: "human_Resources_DB"
+     - executes create department query, with class @param: this.departmentName
+     - catches any errors, and ends the connection.
+    */
     async addDept() {
         try {
             const db = await this.connection("human_Resources_DB")
             const action = await db.query(`INSERT INTO departments (department_name) VALUES ("${this.departmentName}")`)
-            console.log(action)
             console.log(`
                 New department added to the database: ${this.departmentName}
             `)
@@ -24,11 +34,16 @@ class Department {
         }
     }
 
-    async deleteDept () {
-        try{
+    /*
+     addDept method: 
+    - creates connection to database: "human_Resources_DB"
+    - executes delete department query, with class @param: this.departmentName
+    - catches any errors, and ends the connection.
+*/
+    async deleteDept() {
+        try {
             let db = await this.connection("human_Resources_DB")
-            let action = db.query(`DELETE FROM departments WHERE department_name = ${this.departmentName}`)
-            console.log(action)
+            let action = db.query(`DELETE FROM departments WHERE department_name = "${this.departmentName}"`)
             console.log(`
                 Department: ${this.departmentName} has been deleted from the database!
             `)
@@ -43,6 +58,3 @@ class Department {
 
 }
 
-const test = new Department("Test")
-
-console.log(test.addDept())
