08c50f0bb3b890b059ce4ec9c5cd92e9dc0cad57
Larry_A <larryagbana@gmail.com>
2020-05-05 12:16:45 -0400

    added getEmployeeNames method

--- dao/Employee.js
+++ dao/Employee.js
@@ -18,6 +18,7 @@ class Employee {
         } else {
             this.managerID = options.managerID
         }
+        this.managerName = options.managerName
         this.employeeID = options.employeeID
     }
     /*
@@ -47,7 +48,7 @@ class Employee {
         (
             SELECT ID, First_Name, Last_Name
             FROM joined_table 
-            WHERE title = "Manager"
+            WHERE title LIKE '%Manager'
         ) as temp
         SET Manager = CONCAT(temp.First_Name, " ", temp.Last_Name) WHERE joined_table.Manager_ID = temp.ID AND Manager_ID is not NULL;
         `)
@@ -87,7 +88,7 @@ class Employee {
     async updateEmployeeRole() {
         try {
             let db = await this.connection("human_Resources_DB")
-            let action = db.query(`UPDATE employees SET role_ID = "${this.roleID}", manager_ID = ${this.managerID} WHERE id = ${this.employeeID};`)
+            let action = await db.query(`UPDATE employees SET role_ID = "${this.roleID}", manager_ID = ${this.managerID} WHERE id = ${this.employeeID};`)
             await this.joinTable()
         } catch (err) {
             console.log(err)
@@ -103,7 +104,7 @@ class Employee {
     async updateEmployeeManager() {
         try {
             let db = await this.connection("human_Resources_DB")
-            let action = db.query(`UPDATE employees SET manager_ID = ${this.managerID} WHERE id = ${this.employeeID};`)
+            let action = await db.query(`UPDATE employees SET manager_ID = ${this.managerID} WHERE id = ${this.employeeID};`)
             await this.joinTable()
         } catch (err) {
             console.log(err)
@@ -119,7 +120,7 @@ class Employee {
     async deleteEmployee() {
         try {
             let db = await this.connection("human_Resources_DB")
-            let action = db.query(`DELETE FROM employees WHERE first_name = "${this.firstName}" AND last_name = "${this.lastName}" AND id = "${this.employeeID}";`)
+            let action = await db.query(`DELETE FROM employees WHERE first_name = "${this.firstName}" AND last_name = "${this.lastName}" AND id = "${this.employeeID}";`)
             await this.joinTable()
         } catch (err) {
             console.log(err)
@@ -131,7 +132,87 @@ class Employee {
             db.end()
         }
     }
+    /*
+     *getManager method: 
+        - executes get manager name query, with class @param: this.managerID
+    */
+    async getManagerID() {
+        try {
+            let db = await this.connection("human_Resources_DB")
+            let [[action]] = await db.query(`SELECT DISTINCT Manager_ID FROM joined_table WHERE Manager = "${this.managerName}";`)
+            return action.Manager_ID
+        } catch (err) {
+            console.log(err)
+        } finally {
+            const db = await this.connection("human_Resources_DB")
+            db.end()
+        }
+    }
+    /*
+    *getManagerNames method: 
+    - executes SELECT query on the joined table
+     *return = []
+        # an array of all manager Names
+    */
+    async getManagerNames() {
+        try {
+            let results = []
+            let db = await this.connection("human_Resources_DB")
+            let [response] = await db.query(`SELECT DISTINCT concat(First_Name, " ", Last_Name) AS "manager" FROM joined_table WHERE title LIKE '%Manager';`)
+            for (let row of response) {
+                results.push(row.manager)
+            }
+            return results
+        } catch (err) {
+            console.log(err)
+        } finally {
+            const db = await this.connection("human_Resources_DB")
+            db.end()
+        }
+    }
+    // select first_name, last_name from employees where id = 5;
+    /*
+    *getEmployeeNames method: 
+    - executes SELECT query on the joined table
+     *return = { [], [], [] }
+        # an object of arrays containing full names, first names, last names
+    */
+   async getEmployeeNames() {
+    try {
+        let names = [], firstNames = [], lastNames = []
+        let db = await this.connection("human_Resources_DB")
+        let [response] = await db.query(`SELECT CONCAT(first_name, " ", last_name) AS name, first_name, last_name FROM employees;`)
+        for (let row of response) {
+            names.push(row.name)
+            firstNames.push(row.first_name)
+            lastNames.push(row.last_name)
+        }
+        let results = {names: names, firstNames: firstNames, lastNames: lastNames}
+        return results
+    } catch (err) {
+        console.log(err)
+    } finally {
+        const db = await this.connection("human_Resources_DB")
+        db.end()
+    }
+}
+    /*
+    *getEmployeeID method: 
+    - executes SELECT query on the employees table
+     *return = Integer, employee ID
+    */
+   async getEmployeeID() {
+    try {
+        let db = await this.connection("human_Resources_DB")
+        let [[response]] = await db.query(`SELECT id from employees WHERE first_name = "${this.firstName}" AND last_name = "${this.lastName}";`)
+        return response.id
+    } catch (err) {
+        console.log(err)
+    } finally {
+        const db = await this.connection("human_Resources_DB")
+        db.end()
+    }
+}
 }
 module.exports = Employee
-
 
